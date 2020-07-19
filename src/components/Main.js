eeb206ba5ca4cbab52909f9b1ebe6128f362e74f
Max Sh <realmxs@icloud.com>
2020-07-19 18:40:28 +0300

    Project 10 (v10.3) {main.js fixes}

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,16 @@ import { api } from '../utils/api.js';
 import Card from './Card.js'
 
 function Main(props) {
-  const [name, setName] = React.useState();
-  const [description, setDescription] = React.useState();
-  const [avatar, setAvatar] = React.useState();
+  const [userInfo, setUserInfo] = React.useState([]);
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     api
       .getUserInfo()
-      .then((user) => {
-        setName(user.name);
-        setDescription(user.about);
-        setAvatar(user.avatar);
+      .then(user => {
+        setUserInfo(user)
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, []);
@@ -24,10 +20,10 @@ function Main(props) {
   React.useEffect(() => {
     api
       .getDefaultCards()
-      .then((cards) => {
+      .then(cards => {
         setCards(cards);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, []);
@@ -38,14 +34,14 @@ function Main(props) {
       <section className="profile">
         <div className="profile__container">
           <button onClick={props.editAvatar} className="profile__avatar-button">
-            <img className="profile__avatar" alt="Ваше фото" src={avatar} />
+            <img className="profile__avatar" alt="Ваше фото" src={userInfo.avatar}/>
           </button>
           <div className="profile__info">
             <div className="profile__wrapper">
-              <h1 className="profile__title">{name}</h1>
+              <h1 className="profile__title">{userInfo.name}</h1>
               <button className="profile__edit-button" type="button" onClick={props.editProfile}></button>
             </div>
-            <p className="profile__subtitle">{description}</p>
+  <p className="profile__subtitle">{userInfo.about}</p>
           </div>
         </div>
         <button className="open-newcard-popup-button" onClick={props.addPlace}></button>
