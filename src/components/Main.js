2b6748d0318ba72bb553b5bd2df6d6e3e410b0e7
Max Sh <realmxs@icloud.com>
2020-07-19 21:10:59 +0300

    Project 10 (v1.4) {main.js fixes)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { api } from '../utils/api.js';
-import Card from './Card.js'
+import React from "react";
+import { api } from "../utils/api.js";
+import Card from "./Card.js";
 
 function Main(props) {
-  const [userInfo, setUserInfo] = React.useState([]);
+  const [name, setName] = React.useState();
+  const [description, setDescription] = React.useState();
+  const [avatar, setAvatar] = React.useState();
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
     api
       .getUserInfo()
-      .then(user => {
-        setUserInfo(user)
+      .then((user) => {
+        setName(user.name);
+        setDescription(user.about);
+        setAvatar(user.avatar);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
@@ -20,31 +24,41 @@ function Main(props) {
   React.useEffect(() => {
     api
       .getDefaultCards()
-      .then(cards => {
+      .then((cards) => {
         setCards(cards);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
-
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__container">
           <button onClick={props.editAvatar} className="profile__avatar-button">
-            <img className="profile__avatar" alt="Ваше фото" src={userInfo.avatar}/>
+            <img
+              className="profile__avatar"
+              alt="Ваше фото"
+              src={avatar}
+            />
           </button>
           <div className="profile__info">
             <div className="profile__wrapper">
-              <h1 className="profile__title">{userInfo.name}</h1>
-              <button className="profile__edit-button" type="button" onClick={props.editProfile}></button>
+              <h1 className="profile__title">{name}</h1>
+              <button
+                className="profile__edit-button"
+                type="button"
+                onClick={props.editProfile}
+              ></button>
             </div>
-  <p className="profile__subtitle">{userInfo.about}</p>
+            <p className="profile__subtitle">{description}</p>
           </div>
         </div>
-        <button className="open-newcard-popup-button" onClick={props.addPlace}></button>
+        <button
+          className="open-newcard-popup-button"
+          onClick={props.addPlace}
+        ></button>
       </section>
 
       <section className="cards-container">
@@ -53,7 +67,7 @@ function Main(props) {
         ))}
       </section>
     </main>
-  )
+  );
 }
 
-export default Main
+export default Main;
