55aec0ea25be421ecec77bacfc03ff89368aca06
Екатерина Воскобойник <frosyakate@MacBook-Pro-Ekaterina.local>
2020-07-21 22:54:21 +0300

    Updates

---
+++ precache-manifest.70109d6a5e500b36fdbad5e585831f3b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cad2e9f34c922a384f4c538a6fc4e2f",
+    "url": "/mesto/index.html"
+  },
+  {
+    "revision": "9762f776e7b4a2237c71",
+    "url": "/mesto/static/css/main.127510b7.chunk.css"
+  },
+  {
+    "revision": "c26a666d2b16bb335c29",
+    "url": "/mesto/static/js/2.5b0fe9dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto/static/js/2.5b0fe9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9762f776e7b4a2237c71",
+    "url": "/mesto/static/js/main.0a092119.chunk.js"
+  },
+  {
+    "revision": "738a12e212008e0b74fd",
+    "url": "/mesto/static/js/runtime-main.db004d61.js"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/mesto/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/mesto/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "/mesto/static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/mesto/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "3f74383948676e207ccf7177e782cfec",
+    "url": "/mesto/static/media/card__delete-button.3f743839.svg"
+  },
+  {
+    "revision": "e7a8906a3b0887219bad0d62de4f7612",
+    "url": "/mesto/static/media/card__like-button.e7a8906a.svg"
+  },
+  {
+    "revision": "b12167400da3396be4b9b6e69ba27f5d",
+    "url": "/mesto/static/media/card__like-button_clicked.b1216740.svg"
+  },
+  {
+    "revision": "697f7460b25fdd406c3928ff95538229",
+    "url": "/mesto/static/media/logo.697f7460.svg"
+  },
+  {
+    "revision": "50cf486163827b9b62c764268a435184",
+    "url": "/mesto/static/media/open-newcard-popup-button.50cf4861.svg"
+  },
+  {
+    "revision": "4056b612a59cf5b2722bc52744a46b58",
+    "url": "/mesto/static/media/popup__close-button.4056b612.svg"
+  },
+  {
+    "revision": "b9b65c7e7e563b8df5ac93cc01872a72",
+    "url": "/mesto/static/media/profile__edit-button.b9b65c7e.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto/static/media/profile__pic-edit.d476a8b8.svg"
+  }
+]);
--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const loginInfo = {
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-12",
-  headers: {
-    authorization: "246d132c-15dc-4567-a2e3-3aa37ea260d6",
-    "Content-Type": "application/json",
-  },
-};
-
-
