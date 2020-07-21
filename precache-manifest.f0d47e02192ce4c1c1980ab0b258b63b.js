1abb251b67e8149bc61476cfdf986a068e422138
Екатерина Воскобойник <frosyakate@MacBook-Pro-Ekaterina.local>
2020-07-21 23:54:11 +0300

    Updates

---
+++ precache-manifest.f0d47e02192ce4c1c1980ab0b258b63b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834b8173d511173024826b175e9403b8",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "956a42c35b321991aa6c",
+    "url": "/mesto-react/static/css/main.0383d02d.chunk.css"
+  },
+  {
+    "revision": "c26a666d2b16bb335c29",
+    "url": "/mesto-react/static/js/2.5b0fe9dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.5b0fe9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956a42c35b321991aa6c",
+    "url": "/mesto-react/static/js/main.5028e5ef.chunk.js"
+  },
+  {
+    "revision": "b653025071dc48eddfc2",
+    "url": "/mesto-react/static/js/runtime-main.937ca5b9.js"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/mesto-react/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/mesto-react/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "/mesto-react/static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/mesto-react/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "3f74383948676e207ccf7177e782cfec",
+    "url": "/mesto-react/static/media/card__delete-button.3f743839.svg"
+  },
+  {
+    "revision": "e7a8906a3b0887219bad0d62de4f7612",
+    "url": "/mesto-react/static/media/card__like-button.e7a8906a.svg"
+  },
+  {
+    "revision": "b12167400da3396be4b9b6e69ba27f5d",
+    "url": "/mesto-react/static/media/card__like-button_clicked.b1216740.svg"
+  },
+  {
+    "revision": "697f7460b25fdd406c3928ff95538229",
+    "url": "/mesto-react/static/media/logo.697f7460.svg"
+  },
+  {
+    "revision": "50cf486163827b9b62c764268a435184",
+    "url": "/mesto-react/static/media/open-newcard-popup-button.50cf4861.svg"
+  },
+  {
+    "revision": "4056b612a59cf5b2722bc52744a46b58",
+    "url": "/mesto-react/static/media/popup__close-button.4056b612.svg"
+  },
+  {
+    "revision": "b9b65c7e7e563b8df5ac93cc01872a72",
+    "url": "/mesto-react/static/media/profile__edit-button.b9b65c7e.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto-react/static/media/profile__pic-edit.d476a8b8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74966fca704ef1820cbd5aa6f7b0f1a6.js"
+  "/mesto-react/precache-manifest.f0d47e02192ce4c1c1980ab0b258b63b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mesto-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
