ea5955a47d155283e2c85fdd2ca09564592aea31
Max Sh <realmxs@icloud.com>
2020-08-02 18:54:24 +0300

    Project 11 (v1.5)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react';
 function Footer(props) {
   return (
     <footer className="footer">
-      <a href="#">
-        <p className="footer__copyright" lang="en">&copy;2020 Mesto Russia</p>
-      </a>
+      <p className="footer__copyright" lang="en">&copy;2020 Mesto Russia</p>
     </footer>
   )
 }
